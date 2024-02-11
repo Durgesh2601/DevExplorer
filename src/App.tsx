ff2768076182939fa1347ac92e153c0d7b003b44
Durgesh2601 <dk829445@gmail.com>
2024-02-12 02:04:08 +0530

    feat: error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,29 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
+import { UserProvider } from "./context/UserDataContext";
 import customTheme from "./theme";
 import PageHeader from "./components/PageHeader/PageHeader";
 import HomePage from "./pages/Homepage";
 import UserDetailPage from "./pages/UserDetailPage";
-import { UserProvider } from "./context/UserDataContext";
+import ErrorBoundary from "./components/ErrorBoundary";
+import Fallback from "./components/FallbackComponent";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={customTheme}>
       <CssBaseline />
       <PageHeader />
-      <UserProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/:username" element={<UserDetailPage />} />
-          </Routes>
-        </Router>
-      </UserProvider>
+      <ErrorBoundary fallback={<Fallback />}>
+        <UserProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/:username" element={<UserDetailPage />} />
+            </Routes>
+          </Router>
+        </UserProvider>
+      </ErrorBoundary>
     </ThemeProvider>
   );
 };
